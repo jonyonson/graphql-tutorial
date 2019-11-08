37282491e3600d8da549fdb32a18bc9c1a16e3d5
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-11-07 19:23:18 -0600

    add react app

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BookList() {
+  return (
+    <div>
+      <div id="book-list">
+        <li>Book name</li>
+      </div>
+    </div>
+  );
+}
+
+export default BookList;
