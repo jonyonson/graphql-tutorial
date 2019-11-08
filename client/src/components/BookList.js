7564bd183ae5cd0d996f4353cca69154df268e24
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-11-07 22:15:29 -0600

    add BookDetails component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
 import { getBooksQuery } from '../queries/queries';
 
+// components
+import BookDetails from './BookDetails';
+
 function BookList(props) {
+  const [selected, setSelected] = useState(null);
+
   const displayBooks = () => {
     const data = props.data;
     if (data.loading) {
       return <div>Loading books...</div>;
     } else {
-      return data.books.map((book) => <li key={book.id}>{book.name}</li>);
+      return data.books.map((book) => (
+        <li key={book.id} onClick={() => setSelected(book.id)}>
+          {book.name}
+        </li>
+      ));
     }
   };
   return (
     <div>
-      <div id="book-list">{displayBooks()}</div>
+      <ul id="book-list">{displayBooks()}</ul>
+      <BookDetails bookId={selected} />
     </div>
   );
 }
